24b6ba4c1566031c8f8765e0099c64b50c586bd3
HRAshton <12210721+HRAshton@users.noreply.github.com>
2026-02-07 17:40:22 +0100

    Reformat files

--- packages/app/src/handlers/on-webhook/command-dtos/command-dto.ts
+++ packages/app/src/handlers/on-webhook/command-dtos/command-dto.ts
@@ -1,20 +1,20 @@
 ﻿import type { CommandShortcut } from './command-shortcut';
 
 export abstract class CommandDto {
-    protected abstract command: CommandShortcut;
-    protected abstract properties: (keyof this)[];
+  protected abstract command: CommandShortcut;
+  protected abstract properties: (keyof this)[];
 
-    public static fromPayload(payload: string): CommandDto | null {
-      try {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        return new (this as any)(...payload.split(' ').slice(1));
-      } catch (e) {
-        console.info(e);
-        return null;
-      }
+  public static fromPayload(payload: string): CommandDto | null {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return new (this as any)(...payload.split(' ').slice(1));
+    } catch (e) {
+      console.info(e);
+      return null;
     }
+  }
 
-    public toString(): string {
-      return [this.command, ...this.properties.map(prop => this[prop])].join(' ');
-    }
+  public toString(): string {
+    return [this.command, ...this.properties.map(prop => this[prop])].join(' ');
+  }
 }
